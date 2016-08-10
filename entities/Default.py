316a53cb71904316d9934cdda51685bc58e7a530
Paulo <pacarvalho@wpi.edu>
2016-08-10 13:48:36 -0300

    Added an abstract class to serve as interface for entities.

--- entities/Default.py
+++ entities/Default.py
@@ -6,8 +6,9 @@
 	By Katie and Paulo
 '''
 from PIL import ImageTk
+from EntityBase import EntityBase
 
-class Default:
+class Default(EntityBase):
 	def __init__(self):
 		# Does this cell block others from stepping on it or going through?
 		self.blocking = False
@@ -20,11 +21,18 @@ class Default:
 		return 0
 
 	# Returns the image of the current state
-	def get_default_icon(self):
+	def get_icon(self):
 		return self.default_icon
 
-	# Returns the image of the current state
-	def get_highlight_icon(self):
-		return self.default_icon
+	def get_hitpoints(self):
+		''' Returns the current hitpoints of the entity '''
+		return 100
 
+	def get_selected(self):
+		''' Returns true if entity is currently selected '''
+		return self.isClicked
+
+	def set_selected(self, state):
+		''' Sets if the entity is or not selected '''
+		return False
 
--- entities/Tree.py
+++ entities/Tree.py
@@ -7,27 +7,41 @@
 '''
 from PIL import ImageTk
 
-class Tree:
+class Tree(EntityBase):
 	def __init__(self):
 		# Does this cell block others from stepping on it or going through?
 		self.blocking = True
 
 		# default icon
 		self.default_icon = ImageTk.PhotoImage(file='entities/icons/tree.gif')
-
-		# highlight icon
 		self.highlight_icon = ImageTk.PhotoImage(file='entities/icons/tree_highlight.gif')
+
+		# Is the entity currently clicked?
+		self.isClicked = False
 
 	# Returns the action range of this entity
 	def get_range(self):
 		return 0
 
 	# Returns the image of the current state
-	def get_default_icon(self):
+	def get_icon(self):
+		if(not self.isClicked):
+			return self.default_icon
+		else:
+			return self.highlight_icon
+
 		return self.default_icon
 
-	# Returns the image of the current state
-	def get_highlight_icon(self):
-		return self.highlight_icon
+	def get_hitpoints(self):
+		''' Returns the current hitpoints of the entity '''
+		return 150
+
+	def get_selected(self):
+		''' Returns true if entity is currently selected '''
+		return self.isClicked
+
+	def set_selected(self, state):
+		''' Sets if the entity is or not selected '''
+		self.isClicked = False
 
 

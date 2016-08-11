8a9ecf699f025b9bb8d3286729faa68897002c48
Katie <Katie.gandomi@gmail.com>
2016-08-11 18:22:00 -0300

    Added ability to click on entity and display that entities information in the menu if it is selectable

--- Menu.py
+++ Menu.py
@@ -10,10 +10,30 @@ from PIL import ImageTk
 class Menu	:
 	def __init__(self):
 		self.minimap = None
-		self.description = None
 		self.menu_icon = ImageTk.PhotoImage(file='entities/icons/menu.gif')
-		print 'menu'
-		print self.menu_icon
+		self.display_init_text = True
+		self.default_text = "Welcome to Castle Defense !"
 
 	def get_icon(self):
 		return self.menu_icon
+
+	# Returns the text to display on the menu
+	def get_text(self, selected_cells):
+		# To start if nothing has been selected, continue to display default text
+		if(len(selected_cells) == 0 and self.display_init_text):
+			return self.default_text
+
+		# Only when one cell is selected do we display the following
+		elif (len(selected_cells) == 1):
+			# Set this to False so the default text doesnt display any more
+			self.display_init_text = False
+
+			# Get and Display Entity information
+			entity = selected_cells.values()[0].get_entity()
+			description = entity.get_description()
+			hitpoints = entity.get_hitpoints()
+			text = description + "\n\nHitpoints: " + str(hitpoints) 
+			return text
+
+		# Else dont display anything
+		return ""
